d39aeb77ddf3092d14159fc180a64baaea392402
Billy Wallace <wallace.billy1@gmail.com>
2024-03-30 09:25:15 -0600

    Write feature test for US 4

--- spec/features/adoption_applications/new_spec.rb
+++ spec/features/adoption_applications/new_spec.rb
@@ -18,4 +18,17 @@ RSpec.describe "the adoption application new" do
     expect(page).to have_content("Description: I love dogs and want more.")
     expect(page).to have_content("Status: In Progress")
   end
+
+  it "returns you to the form page if you fail to submit any of the form fields" do
+    visit "/adoption_applications/new"
+    fill_in(:name, with: "Billy Wallace")
+    fill_in(:street_address, with: "321 Dog Lover St.")
+    fill_in(:zip_code, with: 80210)
+    fill_in(:description, with: "I love dogs and want more.")
+    click_button("Submit Application")
+
+    expect(page).to have_current_path("/adoption_applications/new")
+    expect(page).to have_content("Please add your city")
+    expect(page).to have_content("Please add your state")
+  end
 end
