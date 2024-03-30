f1f1e928dc57909a33d61fc5013e55ee404218eb
Billy Wallace <wallace.billy1@gmail.com>
2024-03-29 18:08:40 -0600

    Update formatting of test

--- spec/features/adoption_applications/new_spec.rb
+++ spec/features/adoption_applications/new_spec.rb
@@ -3,12 +3,12 @@ require "rails_helper"
 RSpec.describe "the adoption application new" do
   it "has a form that can be filled with application details" do
     visit "/adoption_applications/new"
-    fill_in("Name", with: "Billy Wallace")
-    fill_in("Street Address", with: "321 Dog Lover St.")
-    fill_in("City", with: "Denver")
-    fill_in("State", with: "CO")
-    fill_in("Zip Code", with: 80210)
-    fill_in("Description", with: "I love dogs and want more.")
+    fill_in(:name, with: "Billy Wallace")
+    fill_in(:street_address, with: "321 Dog Lover St.")
+    fill_in(:city, with: "Denver")
+    fill_in(:state, with: "CO")
+    fill_in(:zip_code, with: 80210)
+    fill_in(:description, with: "I love dogs and want more.")
     click_button("Submit Application")
     
     expect(page).to have_current_path("/adoption_applications/:id")
