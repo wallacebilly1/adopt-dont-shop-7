5f5b72c723d65d363cd69564b06a301fb2e2a65e
Billy Wallace <wallace.billy1@gmail.com>
2024-04-01 07:53:20 -0600

    Add test for returning partial matches in application pet search

--- spec/features/adoption_applications/show_spec.rb
+++ spec/features/adoption_applications/show_spec.rb
@@ -83,6 +83,21 @@ RSpec.describe "the adoption application show page" do
     expect(page).to have_button("Adopt")
   end
 
+  it "can return pet names that partially match the search" do
+    ApplicationPet.destroy_all
+    visit "/adoption_applications/#{@adoption_application.id}"
+
+    fill_in("name", with: "Lucille")
+    click_on("find pet")
+
+    expect(page).to have_content("Lucille Bald")
+
+    fill_in("name", with: "Lobs")
+    click_on("find pet")
+
+    expect(page).to have_content("Lobster")
+  end
+
   it "can add a pet to the adoption application" do
     ApplicationPet.destroy_all
 
