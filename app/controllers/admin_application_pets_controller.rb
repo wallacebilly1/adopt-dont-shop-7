8234fe6061aabfc0a6930d9b2f983e960c76df28
Billy Wallace <wallace.billy1@gmail.com>
2024-04-02 08:20:52 -0600

    Create working admin adoption applications show page, other than displaying status after approval or rejection

---
+++ app/controllers/admin_application_pets_controller.rb
@@ -0,0 +1,9 @@
+class AdminApplicationPetsController < ApplicationController
+  def update
+    application_pet = ApplicationPet.find(params[:id])
+
+    application_pet.update(status: "#{params[:status]}")
+
+    redirect_to "/admin/adoption_applications/#{params[:adoption_application_id]}"
+  end
+end
