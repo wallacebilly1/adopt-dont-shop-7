b176f94b7737c337e826840479f84ca9d0ea808c
Billy Wallace <wallace.billy1@gmail.com>
2024-04-02 07:52:49 -0600

    Build skeleton for admin application show page

--- app/controllers/admin_adoption_applications_controller.rb
+++ app/controllers/admin_adoption_applications_controller.rb
@@ -1,4 +1,6 @@
 class AdminAdoptionApplicationsController < ApplicationController
   def show
+    @adoption_application = AdoptionApplication.find(params[:id])
+    @application_pets = @adoption_application.application_pets
   end
 end
